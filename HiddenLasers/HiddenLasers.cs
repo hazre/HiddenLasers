1ced9e8447836eb583404b1f2a179dcdb5a6c888
hazre <37149950+hazre@users.noreply.github.com>
2024-06-04 19:56:38 +0200

    changes

--- HiddenLasers/HiddenLasers.cs
+++ HiddenLasers/HiddenLasers.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using FrooxEngine;
 using HarmonyLib;
 using ResoniteModLoader;
@@ -14,6 +15,10 @@ namespace HiddenLasers
         public static ModConfiguration config;
 
         [AutoRegisterConfigKey] private static ModConfigurationKey<bool> LASER_VISIBLE = new ModConfigurationKey<bool>("Should the laser be shown?", "", () => true);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> ENABLED_DESKTOP = new ModConfigurationKey<bool>("Should the laser be shown in Desktop Mode?", "", () => false);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> ENABLED_USERSPACE = new ModConfigurationKey<bool>("Should the laser be shown in Userspace?", "", () => true);
+
+        public static List<InteractionLaser> lasers = new List<InteractionLaser>();
 
         public override void OnEngineInit()
         {
@@ -23,32 +28,62 @@ namespace HiddenLasers
             Harmony harmony = new Harmony("me.hazre.HiddenLasers");
             harmony.PatchAll();
 
+            config.OnThisConfigurationChanged += (_) =>
+            {
+                foreach (var instance in lasers)
+                {
+                    if (instance != null)
+                    {
+                        UpdateLaser(instance);
+                    }
+                }
+            };
+        }
+
+        public static void UpdateLaser(InteractionLaser __instance)
+        {
+            var laserVisible = config.GetValue(LASER_VISIBLE);
+            if (__instance.Slot.World.IsUserspace()) laserVisible = config.GetValue(ENABLED_USERSPACE);
+            if (!__instance.Slot.ActiveUserRoot.ActiveUser.VR_Active) laserVisible = config.GetValue(ENABLED_DESKTOP);
+            var laser = __instance.Slot.GetComponent<MeshRenderer>().Mesh;
+            var directCursor = __instance.Slot.FindChild("DirectCursor");
+
+            laser.Component.Enabled = laserVisible;
+            directCursor.ActiveSelf_Field.Value = laserVisible;
+        }
+
+        public static void ForceLinkTemp(InteractionLaser __instance, FieldDrive<bool> ____laserVisible, FieldDrive<bool> ____directCursorVisible)
+        {
+            var temp = __instance.Slot.AttachComponent<ValueField<bool>>();
+            var temp2 = __instance.Slot.AttachComponent<ValueField<bool>>();
+            ____laserVisible.ForceLink(temp.Value);
+            ____directCursorVisible.ForceLink(temp2.Value);
         }
 
         [HarmonyPatch(typeof(InteractionLaser))]
         class PatchInteractionLaser
         {
             [HarmonyPostfix]
-            [HarmonyPatch("OnAwake")]
-            static void Postfix(InteractionLaser __instance, FieldDrive<bool> ____laserVisible, FieldDrive<bool> ____directCursorVisible)
+            [HarmonyPatch("OnAttach")]
+            static void OnAttach(InteractionLaser __instance, FieldDrive<bool> ____laserVisible, FieldDrive<bool> ____directCursorVisible)
             {
-                __instance.RunInUpdates(3, () =>
-                {
-                    if (__instance.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser)
-                        return;
+                if (__instance.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser)
+                    return;
 
-                    Slot slot = __instance.Slot;
-                    var temp = slot.AttachComponent<ValueField<bool>>();
-                    var laser = slot.GetComponent<MeshRenderer>().Mesh;
-                    var directCursor = slot.FindChild("DirectCursor");
-                    ____laserVisible.ForceLink(temp.Value);
-                    ____directCursorVisible.ForceLink(temp.Value);
+                ForceLinkTemp(__instance, ____laserVisible, ____directCursorVisible);
+                UpdateLaser(__instance);
 
-
-                    laser.Component.Enabled = config.GetValue(LASER_VISIBLE);
-                    directCursor.ActiveSelf_Field.Value = config.GetValue(LASER_VISIBLE);
-                });
+                lasers.RemoveAll(instance => instance == null);
+                if (!lasers.Contains(__instance)) lasers.Add(__instance);
             }
+        }
+    }
+    [HarmonyPatch(typeof(ConnectorManager), "ThreadCheck")]
+    public class DisableThreadCheck
+    {
+        public static bool Prefix()
+        {
+            return false;
         }
     }
 }
